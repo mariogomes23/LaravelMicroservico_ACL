56825fbf855d08adab15d6bf3f0e1cbfc53adc76
Mario Algebrico <markkennedy232323@gmail.com>
2023-05-03 09:17:08 +0100

    Auth Register com Sanctum

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\RegisterUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Faker\Provider\UserAgent;
+use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
+
+
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+
+    private $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+
+    }
+
+    public function store(RegisterUserRequest $request)
+    {
+
+
+
+        $users = $this->model->create([
+
+            "name" =>$request->name,
+            "email" =>$request->email,
+            "device_name"=>$request->device_name,
+
+            "password" =>Hash::make($request->password)
+        ]);
+
+
+
+           return (new UserResource($users))->additional([
+            'token' => $users->createToken($request->device_name)->plainTextToken
+           ]);
+
+    }
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
+Route::post("/register",[RegisterController::class,"store"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
